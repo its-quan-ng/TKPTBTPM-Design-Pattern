5b368eedb6969ae92ab6375a81777bc2f14ba084
Nguyen Quan <ngquan6704@gmail.com>
2025-10-06 08:02:59 +0700

    add mathProxy real

---
+++ DPM225461_NguyenThiBichQuan_Real11_MathProxy/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225461_NguyenThiBichQuan_Real11_MathProxy
+{
+    public class Math
+    {
+        public double Add(double x, double y) { return x + y; }
+        public double Sub(double x, double y) { return x - y; }
+        public double Mul(double x, double y) { return x * y; }
+        public double Div(double x, double y) { return x / y; }
+    }
+}
---
+++ DPM225461_NguyenThiBichQuan_Real11_MathProxy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225461_NguyenThiBichQuan_Real11_MathProxy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MathProxy proxy = new MathProxy();
+            // Do the math
+            Console.WriteLine("4 + 2 = " + proxy.Add(4, 2));
+            Console.WriteLine("4 - 2 = " + proxy.Sub(4, 2));
+            Console.WriteLine("4 * 2 = " + proxy.Mul(4, 2));
+            Console.WriteLine("4 / 2 = " + proxy.Div(4, 2));
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}

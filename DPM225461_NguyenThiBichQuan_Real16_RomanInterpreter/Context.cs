619fc81a1da4b84bccd60046db7fac0abe59a8ea
Nguyen Quan <ngquan6704@gmail.com>
2025-10-06 08:34:58 +0700

    add roman interpreter real

---
+++ DPM225461_NguyenThiBichQuan_Real16_RomanInterpreter/Context.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225461_NguyenThiBichQuan_Real16_RomanInterpreter
+{
+    public class Context
+    {
+        string input;
+        int output;
+        public Context(string input)
+        {
+            this.input = input;
+        }
+        public string Input
+        {
+            get { return input; }
+            set { input = value; }
+        }
+        public int Output
+        {
+            get { return output; }
+            set { output = value; }
+        }
+    }
+}
---
+++ DPM225461_NguyenThiBichQuan_Real16_RomanInterpreter/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DPM225461_NguyenThiBichQuan_Real16_RomanInterpreter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DPM225461_NguyenThiBichQuan_Real16_RomanInterpreter")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("96bb29e1-985f-4dcb-94a0-0944671c75cb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

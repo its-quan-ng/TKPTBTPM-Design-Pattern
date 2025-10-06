4bb8d7b47fb89419d1596225062ede615d273192
Nguyen Quan <ngquan6704@gmail.com>
2025-10-06 09:31:04 +0700

    add memento pattern

---
+++ DPM225461_NguyenThiBichQuan_Pattern18_Memento/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225461_NguyenThiBichQuan_Pattern18_Memento
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Originator o = new Originator();
+            o.State = "On";
+
+            Caretaker c = new Caretaker();
+            c.Memento = o.CreateMemento();
+
+            o.State = "Off";
+            o.SetMemento(c.Memento);
+            Console.ReadKey();
+        }
+    }
+}

ad13ce0eb75ca0766e4fe8dcba63101fa7c945d2
Nguyen Quan <ngquan6704@gmail.com>
2025-10-06 09:55:01 +0700

    add observer pattern

---
+++ DPM225461_NguyenThiBichQuan_Pattern19_Observer/ConcreteSubject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225461_NguyenThiBichQuan_Pattern19_Observer
+{
+    public class ConcreteSubject : Subject
+    {
+        private string subjectState;
+        public string SubjectState
+        {
+            get { return subjectState; }
+            set { subjectState = value; }
+        }
+    }
+}

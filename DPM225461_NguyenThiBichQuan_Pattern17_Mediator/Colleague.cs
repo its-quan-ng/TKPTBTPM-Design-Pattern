1a0bb6222355f3df0cad22c5ba48bbe5945c7184
Nguyen Quan <ngquan6704@gmail.com>
2025-10-06 09:15:08 +0700

    add mediator pattern

---
+++ DPM225461_NguyenThiBichQuan_Pattern17_Mediator/Colleague.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225461_NguyenThiBichQuan_Pattern17_Mediator
+{
+    public abstract class Colleague
+    {
+        protected Mediator mediator;
+        public Colleague(Mediator mediator)
+        {
+            this.mediator = mediator;
+        }
+    }
+}

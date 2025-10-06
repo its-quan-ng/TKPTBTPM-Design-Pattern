080d45907cc0a8fe9c234d7bd9437542fdedb5b0
Nguyen Quan <ngquan6704@gmail.com>
2025-10-06 09:25:35 +0700

    add mediator chat room real

---
+++ DPM225461_NguyenThiBichQuan_Real17_Chatroom/NonBeatle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225461_NguyenThiBichQuan_Real17_Chatroom
+{
+    public class NonBeatle : Participant
+    {
+        public NonBeatle(string name)
+            : base(name)
+        {
+        }
+        public override void Receive(string from, string message)
+        {
+            Console.Write("To a non-Beatle: ");
+            base.Receive(from, message);
+        }
+    }
+}

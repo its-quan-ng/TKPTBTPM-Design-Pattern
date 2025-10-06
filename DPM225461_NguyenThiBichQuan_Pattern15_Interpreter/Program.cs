7764a148ebcb760cf0b89043f3533d74c563e19f
Nguyen Quan <ngquan6704@gmail.com>
2025-10-06 08:24:36 +0700

    add interpreter pattern

---
+++ DPM225461_NguyenThiBichQuan_Pattern15_Interpreter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225461_NguyenThiBichQuan_Pattern15_Interpreter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Context context = new Context();
+
+            List<AbstractExpression> list = new List<AbstractExpression>();
+
+            list.Add(new TerminalExpression());
+            list.Add(new NonterminalExpression());
+            list.Add(new TerminalExpression());
+            list.Add(new TerminalExpression());
+
+            foreach (AbstractExpression exp in list)
+            {
+                exp.Interpret(context);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

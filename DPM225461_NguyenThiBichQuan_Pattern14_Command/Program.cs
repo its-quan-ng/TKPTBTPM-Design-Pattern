353949472f69501ef2fa87559afef09114eaf332
Nguyen Quan <ngquan6704@gmail.com>
2025-10-06 08:16:17 +0700

    add command pattern

---
+++ DPM225461_NguyenThiBichQuan_Pattern14_Command/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225461_NguyenThiBichQuan_Pattern14_Command
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Receiver receiver = new Receiver();
+            Command command = new ConcreteCommand(receiver);
+            Invoker invoker = new Invoker();
+
+            invoker.SetCommand(command);
+            invoker.ExecuteCommand();
+
+            Console.ReadKey();
+        }
+
+    }
+    }

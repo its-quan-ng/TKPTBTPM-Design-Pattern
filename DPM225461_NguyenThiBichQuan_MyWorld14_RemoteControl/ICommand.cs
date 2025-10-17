b286849d5509ceb37b2f56a23f5f030378d6668b
Nguyen Quan <ngquan6704@gmail.com>
2025-10-17 22:42:23 +0700

    add myworld14

---
+++ DPM225461_NguyenThiBichQuan_MyWorld14_RemoteControl/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225461_NguyenThiBichQuan_MyWorld14_RemoteControl
+{
+    internal interface ICommand
+    {
+        void Execute();
+        void Undo();
+    }
+}

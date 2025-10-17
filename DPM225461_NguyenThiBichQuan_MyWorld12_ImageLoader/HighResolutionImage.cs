ee6b1662a4ad9e366d79f9a1429efb60ee731f02
Nguyen Quan <ngquan6704@gmail.com>
2025-10-17 22:33:22 +0700

    add myworld12

---
+++ DPM225461_NguyenThiBichQuan_MyWorld12_ImageLoader/HighResolutionImage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225461_NguyenThiBichQuan_MyWorld12_ImageLoader
+{
+    internal class HighResolutionImage : IImage
+    {
+        private string filename;
+
+        public HighResolutionImage(string filename)
+        {
+            this.filename = filename;
+            LoadFromDisk();
+        }
+
+        private void LoadFromDisk()
+        {
+            Console.WriteLine("Loading high-resolution image: {0}", filename);
+            Console.WriteLine("Reading from disk...");
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("Image loaded successfully!\n");
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Displaying image: {0}\n", filename);
+        }
+    }
+}

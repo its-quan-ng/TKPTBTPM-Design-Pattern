370d4acade2fcaabb342fa590a74b68a7b60145f
Nguyen Quan <ngquan6704@gmail.com>
2025-10-17 20:16:58 +0700

    add myword02 and myword04

---
+++ DPM225461_NguyenThiBichQuan_MyWorld02_RestaurantMealBuilder/LuxuryMealBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225461_NguyenThiBichQuan_MyWorld02_RestaurantMealBuilder
+{
+    public class LuxuryMealBuilder : MealBuilder
+    {
+        public LuxuryMealBuilder()
+        {
+            meal = new Meal("Luxury Meal");
+        }
+        public override void BuildDessert()
+        {
+            meal["dessert"] = "Chocolate Lava Cake";
+        }
+        public override void BuildDrink()
+        {
+            meal["drink"] = "Champagne";
+        }
+        public override void BuildMainDish()
+        {
+            meal["mainDish"] = "Wagyu Beef Steak";
+        }
+        public override void BuildSideDish()
+        {
+            meal["sideDish"] = "Truffle Mashed Potatoes";
+        }
+    }
+}
---
+++ DPM225461_NguyenThiBichQuan_MyWorld02_RestaurantMealBuilder/Restaurant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225461_NguyenThiBichQuan_MyWorld02_RestaurantMealBuilder
+{
+    public class Restaurant
+    {
+        public void ConstructMeal(MealBuilder builder)
+        {
+            builder.BuildMainDish();
+            builder.BuildSideDish();
+            builder.BuildDrink();
+            builder.BuildDessert();
+        }
+    }
+}

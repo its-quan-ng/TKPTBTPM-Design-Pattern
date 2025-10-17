5724cdb46ab82c938be28f9124874d2c37f71084
Nguyen Quan <ngquan6704@gmail.com>
2025-10-17 22:17:52 +0700

    add myworld10

---
+++ DPM225461_NguyenThiBichQuan_MyWorld10_ComputerStartupSystem/HardDrive.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225461_NguyenThiBichQuan_MyWorld10_ComputerStartupSystem
+{
+    class HardDrive
+    {
+        public string Read(string sector, int size)
+        {
+            Console.WriteLine("HardDrive: Reading {0} bytes from sector {1}", size, sector);
+            return "boot data";
+        }
+    }
+}

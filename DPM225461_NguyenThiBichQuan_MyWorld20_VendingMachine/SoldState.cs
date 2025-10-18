b13631d9a1243604912571f6ccb530a2682cb949
Nguyen Quan <ngquan6704@gmail.com>
2025-10-18 13:42:14 +0700

    add myworld22

---
+++ DPM225461_NguyenThiBichQuan_MyWorld20_VendingMachine/SoldState.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225461_NguyenThiBichQuan_MyWorld20_VendingMachine
+{
+    internal class SoldState : IState
+    {
+        private VendingMachine machine;
+
+        public SoldState(VendingMachine machine)
+        {
+            this.machine = machine;
+        }
+
+        public void InsertCoin()
+        {
+            Console.WriteLine("Please wait, dispensing...");
+        }
+
+        public void SelectProduct()
+        {
+            Console.WriteLine("Please wait, dispensing...");
+        }
+
+        public void Dispense()
+        {
+            machine.ReleaseProduct();
+
+            if (machine.ProductCount > 0)
+            {
+                Console.WriteLine("Ready for next customer\n");
+                machine.SetState(machine.NoCoinState);
+            }
+            else
+            {
+                Console.WriteLine("Machine is now empty!\n");
+            }
+        }
+    }
+}

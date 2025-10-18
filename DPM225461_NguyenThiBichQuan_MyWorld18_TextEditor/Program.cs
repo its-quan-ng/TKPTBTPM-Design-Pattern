dc555da13e6fe85c23fbb769c98ecaed5639ed9e
Nguyen Quan <ngquan6704@gmail.com>
2025-10-18 10:14:18 +0700

    add myworld18

---
+++ DPM225461_NguyenThiBichQuan_MyWorld18_TextEditor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225461_NguyenThiBichQuan_MyWorld18_TextEditor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TextEditor editor = new TextEditor();
+            History history = new History();
+
+            editor.Write("Hello");
+            history.Push(editor.Save());
+
+            editor.Write(" World");
+            history.Push(editor.Save());
+
+            editor.Write("!!!");
+
+            Console.WriteLine("\n--- Undo 1 ---");
+            editor.Restore(history.Pop());
+
+            Console.WriteLine("\n--- Undo 2 ---");
+            editor.Restore(history.Pop());
+
+            Console.ReadKey();
+        }
+    }
+}
